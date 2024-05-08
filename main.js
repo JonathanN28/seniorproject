0ea6d5bc461e68160b60be0f6d2b12e51b0e014a
JonathanN28 <98481216+JonathanN28@users.noreply.github.com>
2024-05-08 04:54:34 +0000

    build

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import * as CANNON from 'cannon-es'
 import CannonDebugger, * as CANNONDebugger from 'cannon-es-debugger'
 
